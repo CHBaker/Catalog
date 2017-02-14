6bdeb8d7e8bb7c6ea3422aa93a9c3fc25834469f
CHBaker <charlesprobaker@mail.com>
2017-02-14 00:41:59 -0700

    ADD Restaurant Names from DB
    use sql alchemy to gain names from database and viewable as links on
    page

--- webserver.py
+++ webserver.py
@@ -1,8 +1,10 @@
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 import cgi
+
+# import db and allow connection and CRUD ability
+from database_setup import Base, Restaurant, MenuItem
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from database_setup import Base, Restaurant, MenuItem
 
 # tells which db engine to talk to
 engine = create_engine('sqlite:///restaurantmenu.db')
@@ -20,56 +22,43 @@ class webServerHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         try:
-            if self.path.endswith("/hello"):
-                self.send_response(200)
-                self.send_header('Content-type', 'text/html')
-                self.end_headers()
-                output = ""
-                output += "<html><body>"
-                output += "<h1>Hello!</h1>"
-                output += '''<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>'''
-                output += "</body></html>"
-                self.wfile.write(output)
-                print output
-                return
-
             if self.path.endswith("/restaurants"):
+                restaurants = session.query(Restaurant).all()
+                output = ""
                 self.send_response(200)
                 self.send_header('Content-type', 'text/html')
                 self.end_headers()
-
-                output = ""
                 output += "<html><body>"
-                output += "<h1>&#161 Hola !</h1>"
-                output += '''<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>'''
+                for restaurant in restaurants:
+                    output += "<div id='rest-name'><a href=''>" + \
+                    		  restaurant.name + "</a></div><br><br><br>"
+
                 output += "</body></html>"
                 self.wfile.write(output)
-                print output
                 return
-
         except IOError:
             self.send_error(404, 'File Not Found: %s' % self.path)
 
-    def do_POST(self):
-        try:
-            self.send_response(301)
-            self.send_header('Content-type', 'text/html')
-            self.end_headers()
-            ctype, pdict = cgi.parse_header(
-                self.headers.getheader('content-type'))
-            if ctype == 'multipart/form-data':
-                fields = cgi.parse_multipart(self.rfile, pdict)
-                messagecontent = fields.get('message')
-            output = ""
-            output += "<html><body>"
-            output += " <h2> Okay, how about this: </h2>"
-            output += "<h1> %s </h1>" % messagecontent[0]
-            output += '''<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>'''
-            output += "</body></html>"
-            self.wfile.write(output)
-            print output
-        except:
-            pass
+	def do_POST(self):
+		try:
+			self.send_response(301)
+			self.send_header('Content-type', 'text/html')
+			self.end_headers()
+			ctype, pdict = cgi.parse_header(
+				self.headers.getheader('content-type'))
+			if ctype == 'multipart/form-data':
+				fields = cgi.parse_multipart(self.rfile, pdict)
+				messagecontent = fields.get('message')
+			output = ""
+			output += "<html><body>"
+			output += " <h2> Okay, how about this: </h2>"
+			output += "<h1> %s </h1>" % messagecontent[0]
+			output += '''<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>'''
+			output += "</body></html>"
+			self.wfile.write(output)
+			print output
+		except:
+			pass
 
 
 def main():
