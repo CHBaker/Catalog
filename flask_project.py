9688d967f45508d09661e25d3eb626b75de0f22a
CHBaker <charlesprobaker@mail.com>
2017-02-19 22:06:37 -0700

    ADD URLs for crud
    URL routes for CRUD operations on menu items

--- flask_project.py
+++ flask_project.py
@@ -2,17 +2,57 @@
 from flask import Flask
 # create instance of flask class with name of app as argument
 app = Flask(__name__)
+# import restuarant db
+from database_setup import Base, Restaurant, MenuItem
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+# start db session
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind = engine)
+session = DBSession()
+
 
 # decorators wrap pre-defined flask functions to execute
-# Helloworld if either route is requested
-@app.route('/')
-@app.route('/hello')
-def HelloWorld():
-	return "Hello World!"
+# if route is requested
+@app.route('/restaurants/<int:restaurant_id>/')
+def restaurantMenu(restaurant_id):
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+    items = session.query(MenuItem).filter_by(restaurant_id=restaurant.id)
+    output = ''
+    for i in items:
+        output += i.name
+        output += '</br>'
+        output += i.price
+        output += '</br>'
+        output += i.description
+        output += '</br>'
+        output += '</br>'
+    return output
+
+
+# Create route for newMenuItem function here
+@app.route('/restaurants/<int:restaurant_id>/new-menu/')
+def newMenuItem(restaurant_id):
+    return "page to create a new menu item. Task 1 complete!"
+
+
+# Create route for editMenuItem function here
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/edit-menu/')
+def editMenuItem(restaurant_id, menu_id):
+    return "page to edit a menu item. Task 2 complete!"
+
+
+# Create a route for deleteMenuItem function here
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/delete-menu/')
+def deleteMenuItem(restaurant_id, menu_id):
+    return "page to delete a menu item. Task 3 complete!"
+
 
 # main app running is named __main__ all others named __name__
 if __name__ == '__main__':
-	# reloads page when code changes
-	app.debug = True
-	# runs unless this file is imported
-	app.run(host = '0.0.0.0', port = 5000)
+    # reloads page when code changes
+    app.debug = True
+    # runs unless this file is imported
+    app.run(host = '0.0.0.0', port = 5000)
