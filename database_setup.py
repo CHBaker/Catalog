53e145d0ceea06cee4970dd037d704d172925ccb
CHBaker <charlesprobaker@mail.com>
2017-02-22 23:27:50 -0700

    BUG FIX var reference b/assignment

--- database_setup.py
+++ database_setup.py
@@ -3,12 +3,12 @@ import sys
 # import alchemy orm database 
 # classes for mapper
 from sqlalchemy import( 
-	Column, ForeignKey, Integer, String)
+    Column, ForeignKey, Integer, String)
 
 # declarative base for 
 # configuration and class code
 from sqlalchemy.ext.declarative import(
-	declarative_base)
+    declarative_base)
 
 # used for foreing key relationships
 from sqlalchemy.orm import relationship
@@ -22,39 +22,51 @@ Base = declarative_base()
 
 
 class Restaurant(Base):
-	__tablename__ = 'restaurant'
+    __tablename__ = 'restaurant'
 
-	id = Column(
-		Integer, primary_key = True
-		)
-	name = Column(
-		String(180), nullable = False
-		)
+    id = Column(
+        Integer, primary_key = True
+        )
+    name = Column(
+        String(180), nullable = False
+        )
 
 
 class MenuItem(Base):
-	__tablename__ = 'menu_item'
+    __tablename__ = 'menu_item'
 
-	name = Column(
-		String(80), nullable = False
-		)
-	id = Column(
-		Integer, primary_key = True
-		)
-	description = Column(
-		String(250)
-		)
-	price = Column(
-		String(8)
-		)
-	course = Column(
-		String(250)
-		)
-	restaurant_id = Column(
-		Integer, ForeignKey('restaurant.id')
-		)
-	restaurant = relationship(Restaurant)
+    name = Column(
+        String(80), nullable = False
+        )
+    id = Column(
+        Integer, primary_key = True
+        )
+    description = Column(
+        String(250)
+        )
+    price = Column(
+        String(8)
+        )
+    course = Column(
+        String(250)
+        )
+    restaurant_id = Column(
+        Integer, ForeignKey('restaurant.id')
+        )
+    restaurant = relationship(Restaurant)
 
+# serialized function to be able to send JSON objects in a
+# serializable format
+    @property
+    def serialize(self):
+
+        return {
+            'name': self.name,
+            'description': self.description,
+            'id': self.id,
+            'price': self.price,
+            'course': self.course,
+        }
 
 ####### end of file #######
 
